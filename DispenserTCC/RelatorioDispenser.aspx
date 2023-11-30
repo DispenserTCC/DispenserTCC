53449ae04d782497d09823b63358a940cc039260
DispenserTCC <trabalho.tcc2023unip@gmail.com>
2023-11-29 21:45:05 -0300

    Jean botões

--- DispenserTCC/RelatorioDispenser.aspx
+++ DispenserTCC/RelatorioDispenser.aspx
@@ -10,8 +10,10 @@
     <link rel="stylesheet" href="CSS/Estilo.css" />
   </head>
   <body style="background: url(img/medicamentos.jpg); background-repeat:no-repeat; background-size:cover;">
-    <div><asp:Button id="btnvoltar" CssClass="back" OnClick="btnLogout_Click" runat="server" text="Voltar ao Menu"/></div>
-     <div><asp:Button id="btnSair" CssClass="exit" OnClick="btnLogout_Click" runat="server" text="Sair"/></div>
+     <div>
+         <asp:Button id="btnexit" CssClass="exit" OnClick="btnLogout_Click" runat="server" text="Sair"/>
+         <asp:Button id="btnback" CssClass="exit" OnClick="btnLogout_Click" runat="server" text="Voltar ao Menu"/>
+     </div>
       <h3>Gerar Relatório</h3>
       <form action="process_login.php" method="POST">
         <div class="form-group">
--- DispenserTCC/RelatorioDispenser.aspx.designer.cs
+++ DispenserTCC/RelatorioDispenser.aspx.designer.cs
@@ -15,22 +15,22 @@ namespace DispenserTCC
     {
 
         /// <summary>
-        /// Controle btnvoltar.
+        /// Controle btnexit.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
         /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnvoltar;
+        protected global::System.Web.UI.WebControls.Button btnexit;
 
         /// <summary>
-        /// Controle btnSair.
+        /// Controle btnback.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
         /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSair;
+        protected global::System.Web.UI.WebControls.Button btnback;
 
         /// <summary>
         /// Controle btnGerarRelatorio.
